258d44905bd29e59af4a9ae93eded81883fe413f
itsfarhankhan28 <famannat@gmail.com>
2023-08-11 10:02:05 +0530

    style(tailwind.config): added different screen sizes for better responsiveness

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xxsm: '350px',
+      xsm: '425px',
+      mobile: '425px',
+      sm: '640px',
+      tablet: '640px',
+      md: '768px',
+      lg: '1024px',
+      laptop: '1024px',
+      xl: '1440px',
+      desktop: '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
